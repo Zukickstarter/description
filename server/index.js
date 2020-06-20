fd4e22cdb77cee59d712711907eeb554d33f4335
yarosz <nick.yarosz@gmail.com>
2020-06-19 21:08:01 -0700

    Initialize server and client files

--- server/index.js
+++ server/index.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const path = require('path');
-var morgan = require('morgan');
+const morgan = require('morgan');
 
 const app = express();
 const port = 3003;
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(morgan('combined'));
-
+app.use(morgan('tiny'));
 
 app.get('/', (req, res) => res.send('Hello from Express!'));
+
+// https://meet.google.com/zqo-saot-zgd?authuser=1
+app.get('/api/description/:id', (req, res) => {
+  res.send(req.params);
+});
 
 app.listen(port, () => console.log(`Description app listening at http://localhost:${port}`));
