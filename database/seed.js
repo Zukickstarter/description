fd24d4e3eeec0d850703519eaed3e12d678f469b
yarosz <nick.yarosz@gmail.com>
2020-06-22 18:37:51 -0700

    Add data generator function

--- database/seed.js
+++ database/seed.js
@@ -2,40 +2,44 @@ const db = require('./index.js');
 const Desc = require('./model.js');
 const faker = require('faker');
 
-let fakeNumber = faker.random.number();
-let img1 = (faker.random.number({'min': 100,'max': 500}))
-let img2 = (faker.random.number({'min': 100,'max': 500}))
-let fakeParagraph = faker.lorem.paragraphs(3);
-let fakeSentence = faker.lorem.sentence();
-
-const sampleDesc = [
-  {
-    id: fakeNumber,
-    story: {
-      text: fakeParagraph,
-      images: [{
-        src: `http://www.fillmurray.com/${img1}/${img1}`,
-        caption: fakeSentence
-      },
-        {
-        src: `http://www.fillmurray.com/${img2}/${img2}`,
-        caption: fakeSentence
-      }]
-    },
-    risks: fakeParagraph,
-  }
-]
-
-const generate100 = ((param) => {
+// create 100 datapoints
+const generate100 = (() => {
+  // store results
   let generated = []
 
+  let fakeNumber = faker.random.number();
+  let img1 = (faker.random.number({'min': 100,'max': 500}))
+  let img2 = (faker.random.number({'min': 100,'max': 500}))
+  let fakeParagraph = faker.lorem.paragraphs(3);
+  let fakeSentence = faker.lorem.sentence();
 
+  for (let i = 0; i < 100; i++) {
+    let sampleDesc = {
+      id: i,
+      story: {
+        text: fakeParagraph,
+        images: [{
+          src: `http://www.fillmurray.com/${img1}/${img1}`,
+          caption: fakeSentence
+        },
+          {
+          src: `http://www.fillmurray.com/${img2}/${img2}`,
+          caption: fakeSentence
+        }]
+      },
+      risks: fakeParagraph,
+    }
+    generated.push(sampleDesc);
+  }
+  return generated;
 })
+let data = generate100()
+console.log(data)
 
 const seedDb = () => {
   // Desc.create(sampleDesc) // <- can't drop an empty db
   Desc.collection.drop()
-    .then(() => Desc.create(sampleDesc))
+    .then(() => Desc.create(data))
     .then(() => console.log('database seeded!'))
     .catch(() => console.error('error seeding db'))
 }
