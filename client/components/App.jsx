dca34ce128b44b29e3f5c203ca1370b5f38fce51
Nicolas Yarosz <nick.yarosz@gmail.com>
2020-06-25 00:38:53 -0700

    Test all object properties

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -5,7 +5,8 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      date: new Date().toLocaleTimeString(),
+      error: null,
+      isLoaded: false,
       id: props.id,
       data: []
     };
@@ -15,20 +16,50 @@ class App extends React.Component {
     axios.get(`/api/description/${this.state.id}`)
       .then(res => {
         this.setState({
+          isLoaded: true,
           data: res.data[0]
         });
-      });
+      },
+      (error) => {
+        this.setState({
+          isLoaded: true,
+          error
+        });
+      }
+      );
   }
 
   render() {
-    console.log(this.state.data.story);
-    return (
-      <div>
-        <h1>Hello from React!🍕</h1>
-        <h2>The id is: {this.state.id}.</h2>
-        <p>some text</p>
-      </div>
-    );
+    const { error, isLoaded, data } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      console.log(data.id);
+      console.log(data.risks);
+      console.log(data.story);
+      console.log(data.story.textTop);
+      console.log(data.story.imageMiddle);
+      console.log(data.story.imageMiddleCaption);
+      console.log(data.story.textBottom);
+      return (
+        <div>
+          <h1>Hello from React!🍕</h1>
+          <h2>The id is: {data.id}.</h2>
+          <div className="story" >
+            <div className="text" >
+              TopText
+              <img src='https://images-na.ssl-images-amazon.com/images/I/710WW1XPhaL._AC_SL1280_.jpg' alt="this is an img caption">
+              </img>
+              <div className="text">
+                BottomText
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    }
   }
 }
 
