79a9577ed8fe9ac13553c7aaddf0c136bb55bede
Nicolas Yarosz <nick.yarosz@gmail.com>
2020-06-24 21:26:09 -0700

    Add URL id Parsing, Data from db to state * From index.js, pass the parsed URL id into app * Send a GET request with the id retrieved from state * Returns a promise with the campaign project description matching the url id

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,15 +4,35 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      date: new Date()
+      date: new Date().toLocaleTimeString(),
+      id: props.id
     };
+  }
+  // this.props.id = id
+
+  // hello this shaw /image desctiption text/image-number/ and that was an image 🐄
+
+  componentDidMount() {
+    this.intervalID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+  componentWillUnmount() {
+    clearInterval(this.intervalID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date().toLocaleString()
+    });
   }
 
   render() {
     return (
       <div>
-        <h1>Hello from React!</h1>
-        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+        <h1>Hello from React!🍕</h1>
+        <h2>It is {this.state.date}.</h2>
       </div>
     );
   }
