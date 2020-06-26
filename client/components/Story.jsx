1e136366c964c5fc12bbc23ac8af7a7b0f964fe5
Nicolas Yarosz <nick.yarosz@gmail.com>
2020-06-26 00:58:33 -0700

    Add more styling of NavButtons

--- client/components/Story.jsx
+++ client/components/Story.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
-import { StyledDesc, NavHeader, SectionHeader, Caption, Img } from '../StyledDiv.js';
+import { StyledDesc, NavHeader, SectionHeader, Caption, Img } from '../StyledDiv';
 
 const Story = (props) => {
   const {story} = props;
   const { textTop, imageMiddle, imageMiddleCaption, textBottom } = story;
   return (
     <StyledDesc>
-      <NavHeader>Story</NavHeader>
+      <NavHeader id="story">Story</NavHeader>
       <div>
         <p>{textTop.replace(/[\r]+/g, '\n')}</p>
         <Img src={imageMiddle} alt={imageMiddleCaption}></Img>
